853dee2df1dcce30a76a9cb334872ca8d2244e03
cloud-computing.git <anjalidevi07.com>
2026-05-13 14:01:30 +0530

    taskcompleted

--- task1/src/App.js
+++ task1/src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>{process.env.REACT_APP_KEY_APPNAME}</h1>
+          <h2>{process.env.REACT_APP_KEY_VERSION}</h2>
+          <h3>{process.env.REACT_APP_KEY_USE_MODE}</h3>
+          <h4>{process.env.REACT_APP_KEY_SECRET}</h4>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,11 +19,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+          Learn React
+        </div>);
 }
 
 export default App;
